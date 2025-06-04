42007d62d6e7e440664134fee281fdfc5e7c781b
Bradley Bossard <bradleybossard@gmail.com>
2025-06-04 23:06:29 +0000

    refactor(types): simplify wasmLoading and workerWasmLoading types
    - Updated EnableWasmLoadingPlugin constructor to accept only a string type.
    - Refined wasmLoading and workerWasmLoading properties across multiple interfaces to include a function type for better flexibility.

--- declarations/WebpackOptions.d.ts
+++ declarations/WebpackOptions.d.ts
@@ -156,14 +156,21 @@ export type EntryRuntime = false | string;
  * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
  */
 export type WasmLoading =
-       | false
-       | WasmLoadingType
-       | WebpackPluginInstance
-       | WebpackPluginFunction;
+	| false
+	| WasmLoadingType
+	| WebpackPluginInstance
+	| WebpackPluginFunction;
 /**
  * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
  */
 export type WasmLoadingType = ("fetch" | "async-node") | string;
+/**
+ * Function acting as plugin.
+ */
+export type WebpackPluginFunction = (
+	this: import("../lib/Compiler"),
+	compiler: import("../lib/Compiler")
+) => void;
 /**
  * An entry point without name.
  */
@@ -424,13 +431,6 @@ export type Name = string;
  * Include polyfills or mocks for various node stuff.
  */
 export type Node = false | NodeOptions;
-/**
- * Function acting as plugin.
- */
-export type WebpackPluginFunction = (
-	this: import("../lib/Compiler"),
-	compiler: import("../lib/Compiler")
-) => void;
 /**
  * Create an additional chunk which contains only the webpack runtime and chunk hash maps.
  */
@@ -1272,6 +1272,16 @@ export interface LibraryCustomUmdObject {
 	root?: string[] | string;
 }
 /**
+ * Plugin instance.
+ */
+export interface WebpackPluginInstance {
+	/**
+	 * The run point of the plugin, required method.
+	 */
+	apply: (compiler: import("../lib/Compiler")) => void;
+	[k: string]: any;
+}
+/**
  * Enable presets of externals for specific targets.
  */
 export interface ExternalsPresets {
@@ -1851,16 +1861,6 @@ export interface Optimization {
 	 * Figure out which exports are used by modules to mangle export names, omit unused exports and generate more efficient code (true: analyse used exports for each runtime, "global": analyse exports globally for all runtimes combined).
 	 */
 	usedExports?: "global" | boolean;
-}
-/**
- * Plugin instance.
- */
-export interface WebpackPluginInstance {
-	/**
-	 * The run point of the plugin, required method.
-	 */
-	apply: (compiler: import("../lib/Compiler")) => void;
-	[k: string]: any;
 }
 /**
  * Options object for splitting chunks into smaller chunks.
--- types.d.ts
+++ types.d.ts
@@ -4063,14 +4063,8 @@ declare interface EnableLibraryPluginOptions {
 	additionalApply?: () => void;
 }
 declare class EnableWasmLoadingPlugin {
-       constructor(
-               type:
-                       | string
-                       | WebpackPluginInstance
-                       | WebpackPluginFunction
-                       | (WebpackPluginInstance | WebpackPluginFunction | string)[]
-       );
-       type: string | WebpackPluginInstance | WebpackPluginFunction | (WebpackPluginInstance | WebpackPluginFunction | string)[];
+	constructor(type: string);
+	type: string;
 
 	/**
 	 * Apply the plugin
@@ -4175,7 +4169,11 @@ declare interface EntryDescription {
 	/**
 	 * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
 	 */
-        wasmLoading?: string | WebpackPluginInstance | WebpackPluginFunction | false;
+	wasmLoading?:
+		| string
+		| false
+		| ((this: Compiler, compiler: Compiler) => void)
+		| WebpackPluginInstance;
 }
 
 /**
@@ -4235,7 +4233,11 @@ declare interface EntryDescriptionNormalized {
 	/**
 	 * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
 	 */
-       wasmLoading?: string | WebpackPluginInstance | WebpackPluginFunction | false;
+	wasmLoading?:
+		| string
+		| false
+		| ((this: Compiler, compiler: Compiler) => void)
+		| WebpackPluginInstance;
 }
 type EntryItem = string | string[];
 type EntryNormalized =
@@ -11544,7 +11546,11 @@ declare interface Output {
 	/**
 	 * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
 	 */
-       wasmLoading?: string | WebpackPluginInstance | WebpackPluginFunction | false;
+	wasmLoading?:
+		| string
+		| false
+		| ((this: Compiler, compiler: Compiler) => void)
+		| WebpackPluginInstance;
 
 	/**
 	 * The filename of WebAssembly modules as relative path inside the 'output.path' directory.
@@ -11564,7 +11570,11 @@ declare interface Output {
 	/**
 	 * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
 	 */
-       workerWasmLoading?: string | WebpackPluginInstance | WebpackPluginFunction | false;
+	workerWasmLoading?:
+		| string
+		| false
+		| ((this: Compiler, compiler: Compiler) => void)
+		| WebpackPluginInstance;
 }
 declare interface OutputFileSystem {
 	writeFile: WriteFile;
@@ -11823,7 +11833,11 @@ declare interface OutputNormalized {
 	/**
 	 * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
 	 */
-       wasmLoading?: string | WebpackPluginInstance | WebpackPluginFunction | false;
+	wasmLoading?:
+		| string
+		| false
+		| ((this: Compiler, compiler: Compiler) => void)
+		| WebpackPluginInstance;
 
 	/**
 	 * The filename of WebAssembly modules as relative path inside the 'output.path' directory.
@@ -11843,7 +11857,11 @@ declare interface OutputNormalized {
 	/**
 	 * The method of loading WebAssembly Modules (methods included by default are 'fetch' (web/WebWorker), 'async-node' (node.js), but others might be added by plugins).
 	 */
-       workerWasmLoading?: string | WebpackPluginInstance | WebpackPluginFunction | false;
+	workerWasmLoading?:
+		| string
+		| false
+		| ((this: Compiler, compiler: Compiler) => void)
+		| WebpackPluginInstance;
 }
 declare interface ParameterizedComparator<TArg extends object, T> {
 	(tArg: TArg): Comparator<T>;
